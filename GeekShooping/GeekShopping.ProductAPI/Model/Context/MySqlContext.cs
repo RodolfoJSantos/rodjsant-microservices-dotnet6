1c4f7f7885a2fcfa98f789cf9064285cf5979f54
rodolfo santos <rodjsant@outlook.com>
2022-03-03 00:28:33 -0300

    criação da controller api-product

---
+++ GeekShooping/GeekShopping.ProductAPI/Model/Context/MySqlContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace GeekShopping.ProductAPI.Model.Context
+{
+    public class MySqlContext : DbContext
+    {
+        public MySqlContext() {}
+        public MySqlContext(DbContextOptions <MySqlContext> options) : base(options) {}
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    if (!optionsBuilder.IsConfigured)
+        //    {
+        //        IConfigurationRoot configuration = new ConfigurationBuilder()
+        //           .SetBasePath(Directory.GetCurrentDirectory())
+        //           .AddJsonFile("appsettings.json")
+        //           .Build();
+        //        var connectionString = configuration["MySqlConnection:MySqlConnectionString"];
+        //        optionsBuilder.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0));
+        //    }
+        //}
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
